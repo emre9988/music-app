1861ae64b2e3e4b7c712e3feffd0264e950767b2
emre9988 <emregurlenkaya26@gmail.com>
2022-10-17 04:10:17 +0300

    android music app

---
+++ emremusic/app/src/main/java/com/example/emremusic/MusicPlayerActivity.java
@@ -0,0 +1,182 @@
+package com.example.emremusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class MusicPlayerActivity extends AppCompatActivity {
+
+    TextView titleTv,currentTimeTv,totalTimeTv;
+    SeekBar seekBar;
+    ImageView pausePlay,nextBtn,previousBtn,musicIcon,sleepBtn,likeBtn;
+    ArrayList<AudioModel> songsList;
+    AudioModel currentSong;
+    MediaPlayer mediaPlayer = MyMediaPlayer.getInstance();
+    int x=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+
+        titleTv = findViewById(R.id.song_title);
+        currentTimeTv = findViewById(R.id.curren_time);
+        totalTimeTv = findViewById(R.id.total_time);
+        seekBar = findViewById(R.id.seek_bar);
+        pausePlay = findViewById(R.id.pause_play);
+        nextBtn = findViewById(R.id.next);
+        previousBtn = findViewById(R.id.previous);
+        musicIcon = findViewById(R.id.music_icon_big);
+        sleepBtn = findViewById(R.id.sleep_btn);
+        likeBtn = findViewById(R.id.like_btn);
+
+        likeBtn.setOnClickListener(new View.OnClickListener() {
+            // muzigi begenme  ve uyku zamanlayıcı tusu suanda yapmadıgım icin ekrana yazı cıkartım.
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MusicPlayerActivity.this,"Bu özellik şuanda kullanılmıyor",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sleepBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MusicPlayerActivity.this,"Bu özellik şuanda kullanılmıyor",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        titleTv.setSelected(true);
+
+        songsList = (ArrayList<AudioModel>) getIntent().getSerializableExtra("LIST");
+
+        setResourcesWithMusic();
+
+        MusicPlayerActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(mediaPlayer!=null){
+                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                    currentTimeTv.setText(convertToMMSS(mediaPlayer.getCurrentPosition()+""));
+
+                    if(mediaPlayer.isPlaying()){
+                        pausePlay.setImageResource(R.drawable.ic_baseline_pause_circle_24);
+
+                    }else{
+                        pausePlay.setImageResource(R.drawable.ic_baseline_play_circle_24);
+
+                    }
+
+                }
+                new Handler().postDelayed(this,100);
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(mediaPlayer!=null && fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+    void setResourcesWithMusic(){
+        currentSong = songsList.get(MyMediaPlayer.currentIndex);
+
+        titleTv.setText(currentSong.getTitle());
+
+        totalTimeTv.setText(convertToMMSS(currentSong.getDuration()));
+
+        pausePlay.setOnClickListener(v-> pausePlay());
+        nextBtn.setOnClickListener(v-> playNextSong());
+        previousBtn.setOnClickListener(v-> playPreviousSong());
+
+        playMusic();
+
+
+    }
+
+
+
+
+    private void playMusic(){
+
+        mediaPlayer.reset();
+        try {
+            mediaPlayer.setDataSource(currentSong.getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            seekBar.setProgress(0);
+            seekBar.setMax(mediaPlayer.getDuration());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void playNextSong(){
+
+        if(MyMediaPlayer.currentIndex== songsList.size()-1)
+            return;
+        MyMediaPlayer.currentIndex +=1;
+        mediaPlayer.reset();
+        setResourcesWithMusic();
+
+    }
+
+    private void playPreviousSong(){
+        if(MyMediaPlayer.currentIndex== 0)
+            return;
+        MyMediaPlayer.currentIndex -=1;
+        mediaPlayer.reset();
+        setResourcesWithMusic();
+    }
+
+    private void pausePlay() {
+        if (mediaPlayer.isPlaying())
+            mediaPlayer.pause();
+        else
+            mediaPlayer.start();
+    }
+    private void sleep(View view){
+        Toast.makeText(MusicPlayerActivity.this,"Bu özellik şuanda kullanılmıyor",Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+    public static String convertToMMSS(String duration){
+        Long millis = Long.parseLong(duration);
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
+                TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1));
+    }
+}
